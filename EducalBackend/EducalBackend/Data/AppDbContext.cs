bc17eb70701b351795a3b683c575c5daa5d7a72a
unknown <asgarkhanmb@code.edu.az>
2024-06-03 02:58:20 +0400

    Category-Crud-Task

---
+++ EducalBackend/EducalBackend/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using EducalBackend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EducalBackend.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<CourseImage> CourseImages { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasQueryFilter(m => !m.SoftDeleted);
+            modelBuilder.Entity<Course>().HasQueryFilter(m => !m.SoftDeleted);
+        }
+    }
+}
